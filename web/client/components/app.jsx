68571753ff25fbd3ea3e11225d0d8a7becb41965
alexLipnik1 <alex.lipnik111@gmail.com>
2018-04-28 22:39:42 +0300

    added style to login page

--- web/client/components/app.jsx
+++ web/client/components/app.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Login from './login';
+// import 'bootstrap/dist/css/bootstrap.css';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 export default class App extends React.Component {
     render() {
         return (
-            <Login />
+            <MuiThemeProvider>
+                <Login />
+            </MuiThemeProvider>
         )
     }
 }
--- web/client/components/login/index.jsx
+++ web/client/components/login/index.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
 import styles from './login.scss';
+import RaisedButton from 'material-ui/RaisedButton';
+
+let addressImg = 'https://cdn2.iconfinder.com/data/icons/ios-7-icons/50/user_male2-512.png';
+// let addressImg = '';
+// let addressImg = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUb15YPNWxjd1jx9uuvduvBeE412ak_t-9sIlOk6kt8_59Ow2TKQ';
 
 let Login = () =>{
     return (
@@ -8,6 +13,7 @@ let Login = () =>{
                 <div className={styles.title}>Login</div>
                 <div className={styles.form}>
                     <div className={styles.formContainer}>
+                        <img className={styles.img} src={addressImg} />
                         <div className={styles.inputContainer}>
                             <div className={styles.label}>User Name:</div>
                             <input tipe="text" id="userName" />
@@ -16,9 +22,16 @@ let Login = () =>{
                             <div className={styles.label}>Password:</div>
                             <input tipe="text" id="password" /> 
                         </div>
+                        {/* <Button className={styles.btn}>Login</Button> */}
+                        <RaisedButton 
+                            buttonStyle={{borderRadius: 10}} 
+                            overlayStyle={{borderRadius: 10}} 
+                            labelStyle={{fontWeight: 'bold'}} 
+                            label="Login"
+                            // primary={true}
+                            className={styles.btn} /> 
                     </div>
                 </div>
-                <button>Login</button>
             </div>
         </div>
     )
